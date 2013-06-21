f6880aaf935f7557c0f4891586ed0a4382da4517
AchintyaAshok <achintyaashok92@gmail.com>
2013-06-21 17:30:55 -0400

    Changed connection information/database schemas/etc.

---
+++ extractionAPI/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from extractionAPI.models import *
+
+admin.site.register(Story)
+admin.site.register(Slide)
+admin.site.register(Image)
+
--- extractionAPI/models.py
+++ extractionAPI/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class Story(models.Model):
 	story_id = models.AutoField(primary_key=True)
-	headline = models.CharField(max_length=30)
-	author_firstName = models.CharField(max_length=30)
-	author_lastName = models.CharField(max_length=30)
+	headline = models.CharField(max_length=30, verbose_name="Article Headline")
+	author_firstName = models.CharField(max_length=30, verbose_name="Author's First Name")
+	author_lastName = models.CharField(max_length=30, verbose_name="Author's Last Name")
 	timeCreated = models.DateTimeField(auto_now_add=True, editable=False)
-	numberSlides = models.IntegerField()
-	coverPhoto = models.CharField(max_length=500)						#	This is just a placeholder field-type for now, look up the file upload field
+	#numberSlides = models.IntegerField(verbose_name="Number of Slides")
+	coverPhoto = models.ImageField(verbose_name="Cover Photo", upload_to="CoverPhotos")						#	This is just a placeholder field-type for now, look up the file upload field
 	#storyTags = models.ManyToManyField(Tags, db_table="Story_Tags")	# 	The relationship table between a story and tags associated with it
 	#memberSlides = models.ManyToManyField(Slide, through="Slide_In_Story")
 
@@ -20,12 +20,13 @@ class Story_Tags(models.Model):
 	tag_name = models.ForeignKey(Tags)
 
 class Slide_Types(models.Model):
-	type_name = models.CharField(max_length=15, primary_key=True)
+	type_name = models.CharField(max_length=15, primary_key=True, verbose_name="Type of Slide")
 
 class Slide(models.Model):
 	slide_id = models.AutoField(primary_key=True)
-	typeOfSlide = models.ForeignKey(Slide_Types)				#	Each slide must be designated a type existing in the Slide_Types table
-	story_id = models.ForeignKey(Story)
+	typeOfSlide = models.ForeignKey(Slide_Types, verbose_name="Type of Slide")				#	Each slide must be designated a type existing in the Slide_Types table
+	story_id = models.ForeignKey(Story, verbose_name="Belonging to Story")
+	summary = models.CharField(max_length=250, verbose_name="Description", null=True)
 
 class Slide_In_Story(models.Model):
 	story_id = models.ForeignKey(Story)
@@ -34,12 +35,12 @@ class Slide_In_Story(models.Model):
 
 class Image(models.Model):
 	image_id = models.AutoField(primary_key=True)
-	url = models.CharField(max_length=100)						#	Future: Maybe there's a URL field
-	width = models.IntegerField()
-	height = models.IntegerField()
-	timeTaken = models.DateTimeField(auto_now=False, null=True)			#	They input when the photo was taken? this is an optional value
-	photographer_firstName = models.CharField(max_length=30)
-	photographer_lastName = models.CharField(max_length=30)
+	image = models.ImageField(upload_to="Images", verbose_name="Photo")						#	Future: Maybe there's a URL field
+	width = models.IntegerField(verbose_name="Width (pixels)")
+	height = models.IntegerField(verbose_name="Height (pixels)")
+	timeTaken = models.DateTimeField(auto_now=False, null=True, verbose_name="Time Photo Taken")			#	They input when the photo was taken? this is an optional value
+	photo_credit = models.CharField(max_length=30, verbose_name="Photo Credit")
+	caption = models.CharField(max_length=50, verbose_name="Image Caption")
 
 
 class Image_In_Slide(models.Model):
