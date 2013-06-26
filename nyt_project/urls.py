b24aeffaac781587b9f48108c59098d9ab80a906
Pascal Bruno <tipascal@gmail.com>
2013-06-26 11:35:38 -0400

    Added back URL route for admin page

--- nyt_project/urls.py
+++ nyt_project/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^stories/(?P<last>\d+)/$', views.getStories, name='getStories'),
     url(r'^story/(?P<story_id>\d+)/$', views.getStory, name='getStory'),
 )
